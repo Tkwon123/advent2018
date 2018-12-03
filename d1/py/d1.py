15951bc33ddc5a4feac6cab16a3263d596e33e08
Tim Won <tkw21189@gmail.com>
2018-12-03 21:03:41 +0900

    add part 1 python module

---
+++ d1/py/d1.py
@@ -0,0 +1,8 @@
+from functools import reduce
+
+with open('../input.txt', 'r') as f:
+    content = f.readlines()
+    results = list(map(int,content))
+
+    final = reduce(lambda x, y: x + y, results)
+    print(final)
