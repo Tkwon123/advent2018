6b2138c8a3de3178d4004f0443521ac5d28dd3f8
Tim Won <tkw21189@gmail.com>
2018-12-03 11:47:25 +0900

    init commit

---
+++ js/d1/d1.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const util = require("util");
+const readFile = util.promisify(fs.readFile); // wrap the callback with a promise
+let content = "";
+
+readFile('input.txt').then( output =>{
+  content = output.toString();
+  let formatted = formatRawText(content);
+  let total = formatted.reduce((acc, cv)=>{
+    let subtotal = acc + cv
+    return subtotal
+  })
+  console.log(`The total is: ${total}`);
+})
+
+function formatRawText(string){
+  let array = string.split(/\s+/); // carraige returns
+  let parsedArray = array.map(item =>{
+    return parseInt(item)
+  })
+  return parsedArray
+}
