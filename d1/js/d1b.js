c26c18b415dd1d571a40863b30475416f4fb00a9
Tim Won <tkw21189@gmail.com>
2018-12-03 21:03:22 +0900

    refactor folder structure

--- d1/js/d1b.js
+++ d1/js/d1b.js
@@ -1,17 +1,28 @@
 const fs = require('fs');
-
 const util = require("util");
 const readFile = util.promisify(fs.readFile); // wrap the callback with a promise
-let content = "";
 
-readFile('input.txt').then( output =>{
+let match = false
+let history = new Set();
+let counter = 0
+let acc = 0
+
+readFile('../input.txt').then( output =>{
   content = output.toString();
   let formatted = formatRawText(content);
-  let total = formatted.reduce((acc, cv)=>{
-    let subtotal = acc + cv
-    return subtotal
-  })
-  console.log(`The total is: ${total}`);
+  
+  do {
+    let cv = formatted[counter%formatted.length]
+    acc = acc + cv
+    if (history.has(acc)) {
+      console.log(`Found match at: ${acc}`)
+      match = true
+    } else {
+      counter = counter + 1
+      history.add(acc)
+    }
+  } while (!match)
+  
 })
 
 function formatRawText(string){
