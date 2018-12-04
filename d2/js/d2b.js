14285a3ddb516e3aea8f78b5dd59ab679f23a605
Tim Won <tkw21189@gmail.com>
2018-12-04 19:37:26 +0900

    adding answer for day2 part2 js

---
+++ d2/js/d2b.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+const util = require("util");
+const readFile = util.promisify(fs.readFile); // wrap the callback with a promise
+let content = "";
+
+readFile('../data.txt')
+  .then( output =>{
+    content = output.toString();
+    let arr = formatRawText(content);
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        const charsI = [...arr[i]]
+        const charsJ = [...arr[j]]
+        let diff = charsI.reduce((a, c, i) => {
+          return a + (c === charsJ[i] ? 0 : 1)
+        }, 0)
+        if (diff === 1) {
+          console.log(arr[i])
+          console.log(arr[j])
+        }
+      }
+    }
+  })
+
+function formatRawText(string){
+  let array = string.split('\n'); // carraige returns
+  return array
+}
+
